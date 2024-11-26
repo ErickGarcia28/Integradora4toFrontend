6bd8ba2b72ccbac2b852030bf8d61e1110eed494
JonathanOcampo05 <20233tn116@utez.edu.mx>
2024-11-26 01:18:49 -0600

    Casi la riego

--- src/js/login.js
+++ src/js/login.js
@@ -4,10 +4,15 @@ document.getElementById('loginForm').addEventListener('submit', function(event){
 
     event.preventDefault();
     
-        Swal.fire({
-            icon: 'success',
-            title: 'Registro exitoso',
-            text: 'Gracias por unirte',
-            confirmButtonText: 'Aceptar',
-        })
+    Swal.fire({
+        icon: 'success',
+        title: 'Registro exitoso',
+        text: 'Gracias por unirte',
+        confirmButtonText: 'Aceptar',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Redirigir al usuario a otra página
+            window.location.href = 'index.html';
+        }
+    });
     });
